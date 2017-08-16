492445d4b9c44ba3091e0d5a50ea2521e6f277ff
liushuang <479825880@qq.com>
2017-08-16 21:25:12 +0800

    stream学习

--- src/com/ls/java8/stream/DishTest.java
+++ src/com/ls/java8/stream/DishTest.java
@@ -1,8 +1,10 @@
 package src.com.ls.java8.stream;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -46,8 +48,9 @@ public class DishTest {
     }
 
     @Test
+    //java8写法
     public void demo2() {
-        //java8写法
+
         List<String> lowCaloricDishesName = menu.stream()
                 .filter(d -> d.getCalories() < 400)
                 .sorted(Comparator.comparing(Dish::getCalories))
@@ -56,4 +59,23 @@ public class DishTest {
         System.out.println(lowCaloricDishesName);
     }
 
+    @Test
+    //java8写法,利用多核架构.替换stream()
+    public void demo3() {
+        List<String> lowCaloricDishesName = menu.parallelStream()
+                .filter(d -> d.getCalories() < 400)
+                .sorted(Comparator.comparing(Dish::getCalories))
+                .map(Dish::getName)
+                .collect(toList());//import static java.util.stream.Collectors.toList;
+        System.out.println(lowCaloricDishesName);
+    }
+
+    @Test
+    //流只能被消费一次
+    public void demo4() {
+        List<String> list = Arrays.asList("java8","ls","ll");
+        Stream<String> stream = list.stream();
+        stream.forEach(System.out::println);
+        stream.forEach(System.out::println);//第二次抛出异常 stream has already been operated upon or closed  已经被操作或者关闭了.
+    }
 }
